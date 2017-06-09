976ca6f7263abd8aadd645ecfd07340f2beedecd
Steven Xu <s@stevenxu.xyz>
2017-06-09 00:27:00 +0000

    replace cookies with session

--- express_server.js
+++ express_server.js
@@ -3,10 +3,17 @@ const app = express();
 const PORT = process.env.PORT || 8080; // default port 8080
 const bodyParser = require("body-parser");
 const cookieParser = require('cookie-parser');
+var cookieSession = require('cookie-session');
 const bcrypt = require('bcrypt');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
+app.use(cookieSession({
+  name: 'session',
+  secret: "uhaeontuhntoidgp,rabiurobkdirpaeueohdp,rudiroedue",
+  // Cookie Options
+  maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}));
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -55,10 +62,10 @@ function urlsForUser(user_id) {
 }
 
 app.get("/login", (req, res) => {
-  if (users[req.cookies.user_id]) {
+  if (users[req.session.user_id]) {
     res.redirect("/urls");
   } else {
-    res.render("login", {user: users[req.cookies.user_id]});
+    res.render("login", {user: users[req.session.user_id]});
   }
 });
 
@@ -73,7 +80,7 @@ app.post("/login", (req, res) => {
     }
   }
   if (user) {
-    res.cookie("user_id", user.id);
+    req.session.user_id = user.id;
     res.redirect("/urls");
   } else {
     res.status(403).send("wrong email or password");
@@ -81,12 +88,12 @@ app.post("/login", (req, res) => {
 });
 
 app.post("/logout", (req, res) => {
-  res.clearCookie("user_id");
+  req.session = null;
   res.redirect("/urls");
 });
 
 app.get("/register", (req, res) => {
-  res.render("register", {user: users[req.cookies.user_id]});
+  res.render("register", {user: users[req.session.user_id]});
 });
 
 app.post("/register", (req, res) => {
@@ -100,15 +107,15 @@ app.post("/register", (req, res) => {
   const hash = bcrypt.hashSync(password, 10);
   users[id] = { id, email, hash };
   
-  res.cookie("user_id", id);
+  req.session.user_id = id;
 
   res.redirect("/urls");
 
 });
 
 app.get("/urls/new", (req, res) => {
-  if (req.cookies.user_id) {
-    res.render("urls_new", {user: users[req.cookies.user_id]});
+  if (req.session.user_id) {
+    res.render("urls_new", {user: users[req.session.user_id]});
   } else {
     res.redirect("/login");
   }
@@ -117,7 +124,7 @@ app.get("/urls/new", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { 
     urls: urlDatabase,
-    user: users[req.cookies.user_id]
+    user: users[req.session.user_id]
   };
   res.render("urls_index", templateVars);
 });
@@ -126,7 +133,7 @@ app.post("/urls", (req, res) => {
   console.log(req.body);  // debug statement to see POST parameters
   let key = generateRandomString();
   urlDatabase[key] = {
-    ownerId: req.cookies.user_id,
+    ownerId: req.session.user_id,
     link: req.body.longURL
   };
   res.redirect(`urls/${key}`);         // Respond with 'Ok' (we will replace this)
@@ -135,12 +142,12 @@ app.post("/urls", (req, res) => {
 
 app.get("/urls/:id", (req, res) => {
   const urlEntry = urlDatabase[req.params.id];
-  if (req.cookies.user_id === urlEntry.ownerId) {
+  if (req.session.user_id === urlEntry.ownerId) {
     const id = req.params.id;
     const templateVars = { 
       shortURL: id,
       longURL: urlEntry.link,
-      user: users[req.cookies.user_id]
+      user: users[req.session.user_id]
     };
     res.render("urls_show", templateVars);
   } else {
@@ -150,7 +157,7 @@ app.get("/urls/:id", (req, res) => {
 
 app.post("/urls/:id/delete", (req, res) => {
   const urlEntry = urlDatabase[req.params.id];
-  if (req.cookies.user_id === urlEntry.ownerId) {
+  if (req.session.user_id === urlEntry.ownerId) {
     delete urlDatabase[req.params.id];
     res.redirect('/urls');
   } else {
@@ -160,7 +167,7 @@ app.post("/urls/:id/delete", (req, res) => {
 
 app.post("/urls/:id/update", (req, res) => {
   const urlEntry = urlDatabase[req.params.id];
-  if (req.cookies.user_id === urlEntry.ownerId) {
+  if (req.session.user_id === urlEntry.ownerId) {
     urlDatabase[req.params.id].link = req.body.url;
     res.redirect(`/urls/${req.params.id}`);
   } else {
